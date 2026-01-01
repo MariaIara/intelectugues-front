590e2a99afaba926800014272e828c188faeaa88
Iara Braga <iara@incibrasil.com.br>
2026-01-01 19:33:11 -0300

    feat: new components for app header

---
+++ src/components/ProfilePicture.vue
@@ -0,0 +1,6 @@
+<script setup>
+    import avatar from '@/assets/avatar/avatar-one.png'
+</script>
+<template>
+    <img :src="avatar" alt="" class="w-12">
+</template>
--- src/pages/app/Home.vue
+++ src/pages/app/Home.vue
@@ -1,3 +1,11 @@
+<script setup>
+import Header from "@/components/Header.vue";
+
+</script>
+
 <template>
-  <h1>Home</h1>
+  <div class="bg-linear-to-r from-[#E7F1F5] to-[#F7FCFD] h-screen">
+    <Header />
+    <h1>teste</h1>
+  </div>
 </template>
