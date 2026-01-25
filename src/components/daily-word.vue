630b85dc4529b4136428b987e24a4fcb0b86a377
Iara Braga <iara@incibrasil.com.br>
2026-01-25 18:08:07 -0300

    feat: integration daily word and using component

---
+++ src/components/daily-word.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { api } from '@/lib/api';
+import { Star } from 'lucide-vue-next';
+import { onMounted, reactive, ref } from 'vue';
+
+const loading = ref(false);
+const errors = ref([]);
+
+const data = reactive({
+    date: '',
+    word: '',
+    category: '',
+    meaning: '',
+    synonymous: []
+});
+
+const fetchDailyWord = async () => {
+    try {
+        loading.value = true;
+        const response = await api.get('/words/daily');
+        Object.assign(data, response.data.data);
+        data.date = new Date().toLocaleDateString('pt-BR');
+    } catch (error) {
+        errors.value = error;
+    } finally {
+        loading.value = false;
+    }
+}
+
+onMounted(() => {
+    fetchDailyWord();
+});
+
+</script>
+
+<template>
+    <div class="bg-[#DF786E] border border-[#B2463C] rounded-2xl p-8">
+        <div class="flex justify-between">
+            <h3 class="font-semibold font-[Poppins] text-2xl text-[#320202]">Dicionário</h3>
+            <a href="#" class="text-sm text-[#FFF2E1]">{{ data.date }}</a>
+        </div>
+        <div class="flex justify-between mt-8">
+            <div>
+                <h4 class="font-medium text-2xl text-white">{{ data.word }}</h4>
+                <p class="text-sm text-[#E6E6E6]">{{ data.category }}</p>
+            </div>
+            <Star class="text-[#FFF2E1]" />
+        </div>
+        <p class="text-white mt-6">{{ data.meaning }}</p>
+        <div class="flex mt-4 gap-4">
+            <span v-for="(synonym, index) in data.synonymous" :key="index"
+                class="bg-[#FFF2E1] border border-[#FFDCAB] text-[#545454] rounded-2xl px-4 py-1"> {{ synonym }}
+            </span>
+        </div>
+    </div>
+</template>
--- src/pages/app/Home.vue
+++ src/pages/app/Home.vue
@@ -1,12 +1,13 @@
 <script setup>
 import Achievement from "@/components/Achievement.vue";
 import Button from "@/components/Button.vue";
+import DailyWord from "@/components/daily-word.vue";
 import HeaderApp from "@/components/HeaderApp.vue";
 import RankDiv from "@/components/RankDiv.vue";
 import Title from "@/components/Title.vue";
 import TrackToLeft from "@/components/TrackToLeft.vue";
 import TrackToRight from "@/components/TrackToRight.vue";
-import { BookOpenCheck, CircleStar, Drama, Flame, GraduationCap, Handshake, Medal, NotebookPen, Rocket, Sparkles, Star, TrendingUp, Trophy, Zap } from "lucide-vue-next";
+import { BookOpenCheck, CircleStar, Drama, Flame, GraduationCap, Handshake, Medal, NotebookPen, Rocket, Sparkles, TrendingUp, Trophy, Zap } from "lucide-vue-next";
 
 </script>
 
@@ -43,25 +44,7 @@ import { BookOpenCheck, CircleStar, Drama, Flame, GraduationCap, Handshake, Meda
           </div>
         </div>
       </div>
-      <div id="dictionary" class="bg-[#DF786E] border border-[#B2463C] rounded-2xl p-8">
-        <div class="flex justify-between">
-          <h3 class="font-semibold font-[Poppins] text-2xl text-[#320202]">Dicionário</h3>
-          <a href="#" class="text-sm text-[#FFF2E1]">22 dez 2025</a>
-        </div>
-        <div class="flex justify-between mt-8">
-          <div>
-            <h4 class="font-medium text-2xl text-white">Hipérbole</h4>
-            <p class="text-sm text-[#E6E6E6]">substantivo</p>
-          </div>
-          <Star class="text-[#FFF2E1]" />
-        </div>
-        <p class="text-white mt-6">Ênfase expressiva resultante do exagero da significação.</p>
-        <div class="flex mt-4 gap-4">
-          <span class="bg-[#FFF2E1] border border-[#FFDCAB] text-[#545454] rounded-2xl px-4 py-1">exagero</span>
-          <span class="bg-[#FFF2E1] border border-[#FFDCAB] text-[#545454] rounded-2xl px-4 py-1">aumento</span>
-          <span class="bg-[#FFF2E1] border border-[#FFDCAB] text-[#545454] rounded-2xl px-4 py-1">ampliação</span>
-        </div>
-      </div>
+      <DailyWord />
     </section>
 
     <section id="section-two" class="max-w-360 max-h-fit mx-auto grid grid-cols-[2.3fr_1fr] px-5 mt-6 gap-6">
