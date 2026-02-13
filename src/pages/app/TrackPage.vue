9042db8ad527ca22985e7fb478e72b0d778d6b44
Iara Braga <iara@incibrasil.com.br>
2026-02-13 09:27:17 -0300

    feat: page to ido track and view challenges track

---
+++ src/pages/app/TrackPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import CardTrackPage from "@/components/app/CardTrackPage.vue";
+import HeaderApp from "@/components/HeaderApp.vue";
+
+</script>
+<template>
+    <div class="bg-linear-to-r from-[#E7F1F5] to-[#F7FCFD] h-full pb-6">
+        <HeaderApp />
+        <CardTrackPage />
+    </div>
+</template>
