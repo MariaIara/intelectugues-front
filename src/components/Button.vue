862cb58dbf34c632d9024eec40f1483b6ec207fa
Iara Braga <iara@incibrasil.com.br>
2026-03-29 16:33:40 -0300

    feat: nice effects and button animation

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,14 +1,28 @@
 <script setup>
-const props = defineProps(['title', 'color', 'shadowColor']);
+import { ref } from 'vue'
+
+const props = defineProps(['title', 'color', 'shadowColor'])
+
+const isHover = ref(false)
 </script>
 
 <template>
-    <button type="button"
-        class="block font-[Poppins] w-max px-10 py-2 rounded-lg text-white font-bold cursor-pointer hover:shadow-none hover:translate-y-1 transition-all"
-        :style="{
-            backgroundColor: props.color,
-            boxShadow: `4px 4px 0 ${props.shadowColor}`
-        }">
-        {{ props.title }}
-    </button>
+  <button
+    type="button"
+    @mouseenter="isHover = true"
+    @mouseleave="isHover = false"
+    class="block font-[Poppins] w-max px-10 py-2 rounded-lg text-white font-bold
+           cursor-pointer transition-all duration-150 ease-out"
+    :style="{
+      backgroundColor: props.color,
+      boxShadow: isHover
+        ? `1px 1px 0 ${props.shadowColor}`
+        : `4px 4px 0 ${props.shadowColor}`,
+      transform: isHover
+        ? 'translate(3px, 3px)'
+        : 'translate(0, 0)'
+    }"
+  >
+    {{ props.title }}
+  </button>
 </template>
